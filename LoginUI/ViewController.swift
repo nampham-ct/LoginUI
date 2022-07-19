995f6ecbb37fc0d1c96f254bed28757631c0b1ff
NamPham1906 <61858329+NamPham1906@users.noreply.github.com>
2022-07-19 09:35:51 +0700

    Initial Commit

---
+++ LoginUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginUI
+//
+//  Created by Pham Nam on 19/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
